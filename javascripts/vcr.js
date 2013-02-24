7bb0a177578ffaea13555d0082314765b369dcde
Julian Maunder <julianmattmaunder@gmail.com>
2013-02-24 02:32:55 -0800

    landing page progress

---
+++ javascripts/vcr.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+
+	// Cache the Window object
+	$window = $(window);
+                
+   $('body').each(function(){
+                    
+		$(window).scroll(function() {
+
+				var yPos = (Math.round(($window.scrollTop() * 100) / 1080) * 1080);
+				var coords = '0% -'+ yPos + 'px';
+
+				$('body').css({ backgroundPosition: coords });
+		});
+	});
+
+    $('#bottom').click(function(){
+        $('html, body').animate({scrollTop:$(document).height()}, 4000);
+        return false;
+    });
+
+    $(window).scroll(function() {
+   		if($(window).scrollTop() + $(window).height() == $(document).height()) {
+      	$('html, body').animate({scrollTop: 0 }, 0);
+  		}
+		});
+
+});
