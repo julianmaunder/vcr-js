2fc2ef2484a47ec87550a8cdfb526d7c72c24c50
Julian Maunder <julianmattmaunder@gmail.com>
2013-02-23 18:25:25 -0800

    first commit

---
+++ vhs.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+
+	// Cache the Window object
+	$window = $(window);
+                
+   $('body').each(function(){
+                    
+ 			$(window).scroll(function() {
+
+				var yPos = (Math.round(($window.scrollTop() * 100) / 1080) * 1080);
+				var coords = '0% -'+ yPos + 'px';
+
+				$('body').css({ backgroundPosition: coords });
+		
+		});
+	});
+
+  $('#bottom').click(function() {
+		$("html, body").animate({ scrollTop: $(document).height() }, 5000);
+	});
+
+});
