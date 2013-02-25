066f5589fadb22c910b0e69649de57d2ae286579
Julian Maunder <julianmattmaunder@gmail.com>
2013-02-25 15:11:59 -0800

    testing

--- javascripts/vcr.js
+++ javascripts/vcr.js
@@ -25,7 +25,8 @@ $(document).ready(function(){
     
   //LAST COMMIT DATE
   $.getJSON('https://api.github.com/repos/julianmaunder/vcr-js/commits', function(gitData) {
-  	$('#date').append(gitData[0].commit.author.date + '<br>BY: ' gitData[0].commit.author.name);
+  	$('#date').append(gitData[0].commit.author.date);
+  	$('#date').append('<br>BY: ' gitData[0].commit.author.name);
   });
 
 });
