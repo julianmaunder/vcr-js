5962e4082a6a7b23726b9536ed8814b6d7094841
Julian Maunder <julianmattmaunder@gmail.com>
2013-02-25 07:15:42 -0800

    testing

--- javascripts/vcr.js
+++ javascripts/vcr.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
 
-
-	// Cache the Window object
+	//VCR.JS//
 	$window = $(window);
                 
    $('body').each(function(){
@@ -15,12 +14,18 @@ $(document).ready(function(){
 		});
 	});
 
+  //DEMO
   $('#bottom').click(function(){
-  	$('#main').addClass('content-fixed');
-    $('html, body').stop(true).animate({scrollTop:$(document).height()}, 4000);
+ 	$('#main').addClass('content-fixed');
+  $('html, body').stop(true).animate({scrollTop:$(document).height()}, 4000);
     $('html, body').animate({scrollTop: 0 }, 0, function() {
     	$('#main').removeClass('content-fixed');
     });
   });
     
+  //LAST COMMIT
+  $.getJSON('https://api.github.com/repos/julianmaunder/vcr-js/commits', function(gitResults) {
+  	$('#date').html(gitResults.commit.author.date)
+  });
+
 });
