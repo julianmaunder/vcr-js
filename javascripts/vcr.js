8fdfc2ffd844c1a8fd88149f891d5fbdcf45b935
Julian Maunder <julianmattmaunder@gmail.com>
2013-02-27 04:18:01 -0800

    adjusted meta data

--- javascripts/vcr.js
+++ javascripts/vcr.js
@@ -16,7 +16,7 @@ $(document).ready(function(){
 
   //LAST COMMIT DATE
   $.getJSON('https://api.github.com/repos/julianmaunder/vcr-js/commits', function(gitData) {
-  	$('#date').append((gitData[0].commit.author.date) + '<br>BY ' + (gitData[0].commit.author.name) + '<br>' + (gitData[0].commit.message));
+  	$('#date').append((gitData[0].commit.author.date) + "<br>BY: " + (gitData[0].commit.author.name) + "<br>\'" + (gitData[0].commit.message) + "'");
 	});
 
 });
