3da661d45d8f3898052e0136aeff0456d6f9e562
Julian Maunder <julianmattmaunder@gmail.com>
2013-02-27 03:53:26 -0800

    landing page fixes

--- javascripts/vcr.js
+++ javascripts/vcr.js
@@ -14,15 +14,6 @@ $(document).ready(function(){
 		});
 	});
 
-  //DEMO
-  $('#bottom').click(function(){
- 	$('#main').addClass('content-fixed');
-  $('html, body').stop(true).animate({scrollTop:$(document).height()}, 2000);
-  $('html, body').animate({scrollTop: 0 }, 0, function() {
-  	$('#main').removeClass('content-fixed');
-    });
-  });
-    
   //LAST COMMIT DATE
   $.getJSON('https://api.github.com/repos/julianmaunder/vcr-js/commits', function(gitData) {
   	$('#date').append((gitData[0].commit.author.date) + '<br>BY ' + (gitData[0].commit.author.name));
