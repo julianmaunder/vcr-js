80e478d66f5dd8709f708c344e7fc3a4bea1df59
Julian Maunder <julianmattmaunder@gmail.com>
2013-03-17 01:27:10 -0700

    performance fixes

--- javascripts/vcr.js
+++ javascripts/vcr.js
@@ -1,22 +1,26 @@
 $(document).ready(function(){
 
-	//VCR.JS//
+	//VCR.JS
 	$window = $(window);
-                
-   $('body').each(function(){
-                    
+
+  $('body').each(function(){
+
 		$(window).scroll(function() {
-
-			var yPos = (Math.round(($window.scrollTop() * 100) / 1080) * 1080);
+			// Store scrollTop in variable
+			var scrollPos = $window.scrollTop();
+			// Convert scrollTop to a multiple of 1080 (the height of each frame of the animation)
+			var yPos = (Math.round((scrollPos * 100) / 1080) * 1080);
+			// Store the new background position in a variable
 			var coords = '0% -'+ yPos + 'px';
-
+			// Apply new background position
 			$('body').css({ backgroundPosition: coords });
 		});
+
 	});
 
-  //LAST COMMIT DATE
+  // Last commit date, message and author
   $.getJSON('https://api.github.com/repos/julianmaunder/vcr-js/commits', function(gitData) {
-  	$('#date').append((gitData[0].commit.author.date) + "<br>BY: " + (gitData[0].commit.author.name) + "<br>\'" + (gitData[0].commit.message) + "'");
+		$('#date').append((gitData[0].commit.author.date) + "<br>BY: " + (gitData[0].commit.author.name) + "<br>\'" + (gitData[0].commit.message) + "'");
 	});
 
 });
