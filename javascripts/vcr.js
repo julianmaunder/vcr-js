16908544741eee77d309fffa648c0e92858b0fa7
Julian Maunder <julianmattmaunder@gmail.com>
2013-03-17 05:35:46 -0700

    Improved performance

--- javascripts/vcr.js
+++ javascripts/vcr.js
@@ -1,10 +1,9 @@
 // VCR.JS
 $(document).ready(function(){
-	$window = $(window);
   $('body').each(function(){
 		$(window).scroll(function() {
 			// Store scrollTop value in a variable
-			var scrollPos = $window.scrollTop();
+			var scrollPos = $(window).scrollTop();
 			// Convert scrollTop to a multiple of 1080 (the height of each frame of the animation)
 			var yPos = (Math.round((scrollPos * 100) / 1080) * 1080);
 			// Store the new background position in a variable
