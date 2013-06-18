7425a86ad8c6642e769bc4ebf2b5c3b00f54647a
Julian Maunder <julianmattmaunder@gmail.com>
2013-06-18 01:38:27 -0700

    removed git data

--- javascripts/vcr.js
+++ javascripts/vcr.js
@@ -13,7 +13,7 @@ $(document).ready(function(){
 		});
 	});
   // Last commit date, message and author
-  $.getJSON('https://api.github.com/repos/julianmaunder/vcr-js/commits', function(gitData) {
+ /* $.getJSON('https://api.github.com/repos/julianmaunder/vcr-js/commits', function(gitData) {
 		$('#date').append((gitData[0].commit.author.date) + "<br>BY: " + (gitData[0].commit.author.name) + "<br>\'" + (gitData[0].commit.message) + "'");
 	});
-});
+});*/
